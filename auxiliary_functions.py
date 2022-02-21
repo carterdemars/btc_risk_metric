d3029a6f1a2a802b171f71fb2d7ca09b5fe45607
Carter Demars <demarscarter@yahoo.ca>
2022-02-21 16:34:16 -0500

    v_1.2.0-alpha

--- auxiliary_functions.py
+++ auxiliary_functions.py
@@ -46,8 +46,8 @@ def import_data(urls, api_key):
     # join all data into one dataframe
     data_df = data_df.join(df)
 
-    return data_df.drop(data_df.index[data_df.index < datetime(2012, 1, 1)])
-    #return data_df
+    #return data_df.drop(data_df.index[data_df.index < datetime(2012, 1, 1)])
+    return data_df
 
 def calculate_extension(df):
     # extension from 20 week sma/21 week ema
@@ -66,23 +66,23 @@ def calculate_extension(df):
     below.rename('extension', inplace=True)
     neutral.rename('extension', inplace=True)
 
-    return pd.concat([df, pd.concat([above, below, neutral], axis=0)], axis=1)
+    return pd.concat([above, below, neutral], axis=0)
 
 
 # RESCALING FUNCTIONS
 def rescale_sopr(x):
-    if x > 1.0:
-        x = 1.0
-    elif x < -1.0:
-        x = -1.0
+    if x > 0.6:
+        x = 0.6
+    elif x < -0.8:
+        x = -0.8
     return x
 
 
 def rescale_extension(x):
     if x > 2:
         x = 2
-    if x < -1:
-        x = -1
+    elif x < -1.1:
+        x = 1.1
     return x
 
 def rescale_fair_value(fair_value_extension):
@@ -105,13 +105,18 @@ def objective(x, a, b, c, d):
     return a * np.log(b * x + c) + d
 
 
-def logarithmic_regression(y_data):
-    x_values = [x + 1 for x in range(len(y_data))]
+def logarithmic_regression(y_data, index=None, df_index=None):
+
+    x_values = [x + 1 for x in range(len(y_data))] if index is None else [i+1 for i in index]
     y_values = np.log(y_data)
 
-    popt, _ = curve_fit(objective, x_values, y_values, bounds=((0, 0, 0, -np.inf), (np.inf, 100, 100, np.inf)))
+    popt, _ = curve_fit(objective, x_values, y_values, p0=(5, 0.5, 70, -15), bounds=((0, 0, 0, -1000), (100, 100, 10000, 1000)))
+    #print(popt)
+    if df_index is None:
+        return np.exp(objective(np.array(x_values), popt[0], popt[1], popt[2], popt[3]))
 
-    return np.exp(objective(np.array(x_values), popt[0], popt[1], popt[2], popt[3]))
+    return np.exp(objective(np.array([x + 1 for x in range(len(df_index))]), popt[0], popt[1], popt[2], popt[3]))
+
 
 
 def fair_value_regression(df):
@@ -141,6 +146,7 @@ def fair_value_regression(df):
     plt.tight_layout()
 
     plt.savefig('figs//fair_value_regression_band.png', dpi=300)
+    #plt.show()
     plt.close()
 
     return fair_value
@@ -159,7 +165,7 @@ def regression_bands(df):
     # plot regression bands
     fair_value = logarithmic_regression(df['close'])
 
-    for i in [-0.5, 0, 0.5, 1, 1.5, 2]:
+    for i in [-0.5, 0, 0.5, 1, 1.5, 2, 2.5]:
         ax1.plot(df.index, fair_value * np.exp(i), lw=0.5)
         plt.fill_between(df.index, fair_value * np.exp(i - 0.5), fair_value * np.exp(i), alpha=0.4)
 
@@ -176,36 +182,10 @@ def fair_value_extension(df, fair_value):
     fv_extension = rescale_fair_value(fv_extension)
     fv_extension = preprocessing.MinMaxScaler().fit_transform(np.array(fv_extension).reshape(-1, 1))[:, 0]
 
-    # fig, (ax1, ax2) = plt.subplots(1, 2)
-    # fig.set_figheight(5)
-    # fig.set_figwidth(10)
-    #
-    # ax1.set_title("Bitcoin Long Term Accumulation Zones")
-    # ax1.set_xlabel('Date')
-    # ax1.set_yscale('log')
-    # ax1.set_ylabel('Price')
-    #
-    # ax2.set_title('Long Term Risk Metric')
-    # ax2.set_xlabel('Date')
-    #
-    # colour_map = plt.cm.get_cmap('RdYlGn').reversed()
-    # ax1.scatter(df.index, df['close'], c=fv_extension, cmap=colour_map)
-    #
-    # for i in np.linspace(0.1, 1.0, 10):
-    #
-    #     if round(i, 2) <= 0.3:
-    #         colour = 'green'
-    #     elif round(i, 2) >= 0.7:
-    #         colour = 'red'
-    #     else:
-    #         colour = 'yellow'
-    #
-    #     ax2.fill_between(df.index, i - 0.1, i, alpha=abs(i - 0.5), color=colour)
-    #
-    # ax2.plot(df.index, fv_extension, color='white', alpha= 0.5)
-    # plt.savefig('figs//Bitcoin Long Term Accumulation Zones')
-    # plt.close()
     return fv_extension
+
+def sma_200_extension(df, sma_200):
+    return (df['close']/sma_200).apply(lambda value: value - 1)
 
 def long_term_risk_metric(df, accumulation_score):
     fig, (ax1, ax2) = plt.subplots(1, 2)
@@ -236,5 +216,6 @@ def long_term_risk_metric(df, accumulation_score):
 
     ax2.plot(df.index, accumulation_score, color='white', alpha=0.5)
     plt.savefig('figs//Bitcoin Long Term Accumulation Zones')
+    plt.show()
     plt.close()
 
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import requests
 import json
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,29 +10,29 @@ import math
 from scipy.optimize import curve_fit
 from sklearn import preprocessing
 from auxiliary_functions import rescale_sopr, rescale_extension, calculate_extension, regression_bands, \
-    fair_value_regression, fair_value_extension, import_data, rescale_fair_value, long_term_risk_metric
+    fair_value_regression, fair_value_extension, import_data, long_term_risk_metric, logarithmic_regression, \
+    sma_200_extension
 
 
 pd.set_option("display.max_columns", None)
 
 API_KEY = '257eUgVAg2YcQU6j4GgzyqB9rbj'
-urls = [#'https://api.glassnode.com/v1/metrics/market/price_usd_close',
-        'https://api.glassnode.com/v1/metrics/market/price_usd_ohlc',
+urls = ['https://api.glassnode.com/v1/metrics/market/price_usd_ohlc',
         'https://api.glassnode.com/v1/metrics/indicators/sopr']
 
 
-def plot_figure(y_range, x_data, y_data, title):
-
-    plt.title(title)
-    colour_map = plt.cm.get_cmap('RdYlGn').reversed()
-    plt.scatter(x_data, y_data, c=y_data, cmap=colour_map)
-    plt.ylim(y_range[0], y_range[1])
-    plt.colorbar()
-    plt.xticks(rotation=50)
-    plt.tight_layout()
-    plt.savefig(f'{title}.png')
-    plt.show()
-    plt.close()
+# def plot_figure(y_range, x_data, y_data, title):
+#
+#     plt.title(title)
+#     colour_map = plt.cm.get_cmap('RdYlGn').reversed()
+#     plt.scatter(x_data, y_data, c=y_data, cmap=colour_map)
+#     plt.ylim(y_range[0], y_range[1])
+#     plt.colorbar()
+#     plt.xticks(rotation=50)
+#     plt.tight_layout()
+#     plt.savefig(f'{title}.png')
+#     plt.show()
+#     plt.close()
 
 # def plot_price_data(df):
 #     candles = go.Candlestick(x=df.index, open=df['open'], high=df['high'], low=df['low'], close=df['close'])
@@ -52,25 +52,26 @@ def plot_figure(y_range, x_data, y_data, title):
 
 
 
-
-
-
 def main():
 
     df = import_data(urls, API_KEY)
-
-    df = calculate_extension(df)
-
+    df = pd.concat([df, calculate_extension(df)], axis=1)
+    plt.style.use("dark_background")
 
     # process sopr values
     df['sopr'] = preprocessing.StandardScaler().fit_transform(df['sopr'].values.reshape(-1, 1))[:, 0]
-    df['sopr'] = df['sopr'].rolling(50).mean().apply(rescale_sopr)
+    df['sopr'] = df['sopr'].rolling(30).mean().apply(rescale_sopr)
     df['sopr'] = preprocessing.MinMaxScaler().fit_transform(df['sopr'].values.reshape(-1, 1))[:, 0]
+    #df.loc[df.index < pd.to_datetime(date(2012, 1, 1))]['sopr'] = np.nan
+    # plt.plot(df['sopr'])
+    # plt.show()
 
     # process extension values
     df['extension'] = preprocessing.StandardScaler().fit_transform(df['extension'].values.reshape(-1, 1))[:, 0]
     df['extension'] = df['extension'].apply(rescale_extension)
     df['extension'] = preprocessing.MinMaxScaler().fit_transform(df['extension'].values.reshape(-1, 1))[:, 0]
+    # plt.plot(df['extension'])
+    # plt.show()
 
     # process fear and green index values
     df['fear_and_greed'] = df['fear_and_greed'].apply(lambda x: x/100.0).ewm(span=50, adjust=False).mean()
@@ -78,13 +79,20 @@ def main():
     # plot_figure([accumulation_score.min(), accumulation_score.max()], accumulation_score.dropna().index, accumulation_score.dropna(),
     #             "Accumulation Score")
 
+    # process 200w MA extension
+    sma_200 = df['close'].rolling(1400).mean()
+    sma_200 = sma_200_extension(df, sma_200)
+    sma_200 = sma_200[sma_200 < 1.0]
+    sma_200 = pd.Series(preprocessing.MinMaxScaler().fit_transform(abs(1/sma_200).values.reshape(-1, 1))[:, 0], index=sma_200.index)
+    sma_200.drop(sma_200.index[sma_200 > 0.15], inplace=True)
 
     # LONG TERM BTC OUTLOOK (1 YEAR +)
 
     regression_bands(df)
     fair_value = pd.Series(fair_value_regression(df), index=df.index)
     fve = fair_value_extension(df, fair_value)
-
+    # plt.plot(df.index, fve)
+    # plt.show()
     #
     # # OTHER PLOTTING
     #
@@ -97,9 +105,63 @@ def main():
     # ax2.set_ylabel('Accumulation Score')
     #plt.xlim(datetime(2019,1,1), datetime.today() + timedelta(days=30))
     #plt.ylim(0.5*df[df.index > datetime(2019,1,1)]['close'].min(), 1.25*df[df.index > datetime(2019,1,1)]['close'].max())
+
+    # CALCULATE ACCUMULATION SCORE
+
     accumulation_score = (df['sopr'] + df['extension'] + df['fear_and_greed'] + fve) / 4.0
     accumulation_score[df['fear_and_greed'].isna()] = (df['sopr'] + df['extension'] + fve) / 3.0
+    accumulation_score[df['sopr'].isna()] = (df['extension'] + fve) / 2.0
+    accumulation_score[df.index.isin(sma_200.index)] -= sma_200
+
     long_term_risk_metric(df, accumulation_score)
+
+    #print(df[accumulation_score > 0.8]['close'])
+
+    temp_df = df.reset_index(drop=True)
+    non_bubble_data = temp_df[accumulation_score.reset_index(drop=True) < 0.20]
+    bubble_data = temp_df[accumulation_score.reset_index(drop=True) > 0.9]
+
+    non_bubble_data = logarithmic_regression(non_bubble_data['close'], index=non_bubble_data.index, df_index=df.index)
+    bubble_data = logarithmic_regression(bubble_data['close'], index=bubble_data.index, df_index=df.index)
+
+
+    fig, ax1 = plt.subplots()
+    plt.title("Bitcoin Fair Value Logarithmic Regression Bands")
+    ax1.set_xlabel('Date')
+    ax1.set_ylabel('Price')
+
+    # plot BTC price
+    # colour_map = plt.cm.get_cmap('RdYlGn').reversed()
+    # ax1.scatter(df.index, df['close'], c=accumulation_score, cmap=colour_map)
+    ax1.set_yscale('log')
+    ax1.plot(df['close'], color='white', lw=0.5)
+    #ax1.plot(sma_200, color='white')
+
+    # ax1.plot(df.index, non_bubble_data, color='green', lw=0.5)
+    # ax1.plot(df.index, bubble_data, color='red', lw=0.5)
+
+    for i in [-0.2, 0, 0.2]:
+
+        if i == 0:
+            ax1.plot(df.index, non_bubble_data * np.exp(i), color='green', lw=0.5)
+            ax1.plot(df.index, bubble_data * np.exp(i), color='red', lw=0.5)
+        else:
+            ax1.plot(df.index, non_bubble_data * np.exp(i), color='green', lw=0.25)
+            ax1.plot(df.index, bubble_data * np.exp(i), color='red', lw=0.25)
+
+        if i != -0.2:
+            plt.fill_between(df.index, bubble_data * np.exp(i - 0.2), bubble_data * np.exp(i), alpha=0.4, color='red')
+        plt.fill_between(df.index, non_bubble_data * np.exp(i - 0.2), non_bubble_data * np.exp(i), alpha=0.4, color='green')
+
+
+    plt.xticks(rotation=50)
+    plt.tight_layout()
+
+    #plt.savefig('figs//fair_value_regression_band.png', dpi=300)
+    plt.show()
+    plt.close()
+
+
 
 
 
